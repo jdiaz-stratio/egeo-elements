11874267d4af51a126fe1100bb97fec711bbd4cc
jdiaz-stratio <jdiaz@stratio.com>
2020-06-03 17:34:54 +0200

    fix helper compilation

--- projects/egeo-elements/compileHelpers.js
+++ projects/egeo-elements/compileHelpers.js
@@ -3,10 +3,10 @@ const {execSync} = require('child_process');
 compileMainTheme();
 
 function compileMainTheme() {
-    const pathFrom = `../egeo/src`;
+    const pathFrom = `../../node_modules/@stratio/egeo/theme/`;
     const pathTo = `../../dist/helpers`;
 
     //execSync(`lessc ${pathFrom}/global-vars.less ${pathTo}/main-theme.css`);
     execSync(`mkdir ${pathTo}`);
-    execSync(`node-sass ${pathFrom}/theme/global-vars.scss ${pathTo}/main-theme.css`);
+    execSync(`node-sass ${pathFrom}egeo-theme-stratio.scss ${pathTo}/main-theme.css`);
 }
